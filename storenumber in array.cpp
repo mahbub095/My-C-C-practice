5ab62a2bf352f41ae7c837f69dafec9c267cc825
mahbub095 <mahbub.095@gmail.com>
2022-11-16 14:53:31 +0600

    store in array

---
+++ storenumber in array.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+   int i;
+   int a[10] = {10,2,0,14,43,25,18,1, 5, 6};
+   cout <<"Input list ...\n";
+   for(i = 0; i<10; i++) {
+      cout <<a[i]<<"\t";
+   }
+ 
+return 0;
+}
